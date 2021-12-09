71799d3eb4e47240a56d41e2f973a60ae1575491
Ana Jorge <69881067+anagjorge@users.noreply.github.com>
2021-12-09 10:49:58 -0300

    incluindo model-postagem

---
+++ DozeroHub/src/app/model/Postagem.ts
@@ -0,0 +1,13 @@
+//import { Tema } from './Tema';
+//import { User } from './User';
+//inserir data ?
+
+export class Postagem{
+  public id: number
+  public titulo: string
+  public texto: string
+  public curtida: number
+  public imagem: string
+  public usuario: User
+  public tema: Tema
+}
